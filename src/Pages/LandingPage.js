321e407a071146d4073126d96ae41da9f5d88ca2
poojasriramkumar <poojasriramkumar@gmail.com>
2024-02-13 13:14:08 +0530

    layout

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,16 @@
+function LandingPage() {
+    return (
+      <>
+        <div className="container">
+            <div className="row row1Height">
+            </div>
+            <div className="row row2Height">
+            </div>
+            <div className="row row3Height">
+            </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default LandingPage;
